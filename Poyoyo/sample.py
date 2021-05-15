b84c882a16b6831fdea199793569b50011c2adfb
KaitodotWav <78613734+KaitodotWav@users.noreply.github.com>
2021-05-15 15:51:43 +0800

    Delete Poyoyo directory

--- Poyoyo/sample.py
+++
@@ -1,7 +0,0 @@
-import json
-with open(r"Data\Emoji.json", encoding="utf8") as F:
-    JF = F.read()
-    Json = json.loads(JF)
-    GET = Json["default"]
-    MEDIA = GET["Media"]
-print(MEDIA["Prev"])
